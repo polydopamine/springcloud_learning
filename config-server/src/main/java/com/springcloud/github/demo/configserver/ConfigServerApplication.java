d006b27339747042c92ffd982188d598cfe40222
wsh <wangsihangcc@163.com>
2020-05-13 11:52:39 +0800

    first commit

--- config-server/src/main/java/com/springcloud/github/demo/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/springcloud/github/demo/configserver/ConfigServerApplication.java
@@ -3,10 +3,12 @@ package com.springcloud.github.demo.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
