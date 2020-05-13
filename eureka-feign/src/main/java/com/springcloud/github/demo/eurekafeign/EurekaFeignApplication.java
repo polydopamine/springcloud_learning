e94c0d949dd723259df4d94e55200dd35b471207
wsh <wangsihangcc@163.com>
2020-05-13 11:08:03 +0800

    first commit

---
+++ eureka-feign/src/main/java/com/springcloud/github/demo/eurekafeign/EurekaFeignApplication.java
@@ -0,0 +1,17 @@
+package com.springcloud.github.demo.eurekafeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+public class EurekaFeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaFeignApplication.class, args);
+    }
+
+}
---
+++ eureka-ribbon/src/main/java/com/springcloud/github/demo/eurekaribbon/controller/ConsumerController.java
@@ -0,0 +1,21 @@
+package com.springcloud.github.demo.eurekaribbon.controller;
+
+import com.springcloud.github.demo.eurekaribbon.service.ComputeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class ConsumerController {
+
+    @Autowired
+    ComputeService computeService;
+
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public String add() {
+        return computeService.addService();
+    }
+
+}
